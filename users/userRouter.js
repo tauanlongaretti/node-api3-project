ea2afed3d90d8efaa4a91af4d725188c0582fbf2
tauan-longaretti <tauanlongaretti@gmail.com>
2020-03-17 23:18:35 -0400

    user GET and POST implemented

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,39 @@
 const express = require('express');
 
+const userDb = require('./userDb.js');
+
 const router = express.Router();
 
+// POST - New post
 router.post('/', (req, res) => {
-  // do your magic!
+  const postInfo = req.body;
+
+  userDb.insert(postInfo)
+    .then(post => {
+      if (postInfo) {
+        res.status(201).json({ post })
+      } else {
+        res.status(400).json({ success: false, errorMessage: "Please provide content for the post."})
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ error: "There was an error while saving the post to the database" })
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// GET - Get all posts
 router.get('/', (req, res) => {
-  // do your magic!
+  userDb.get(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Error retrieving the posts" })
+    })
 });
 
 router.get('/:id', (req, res) => {
